3ffadcc4f462f230ed13f6794eecca890b45dc06
zzq89757 <1604653835@qq.com>
2024-08-15 16:22:03 +0800

    add dp

--- all_dp.py
+++ all_dp.py
@@ -7,15 +7,15 @@ def reverse_complement(seq: str) -> str:
     return seq.translate(trantab)[::-1]
 
 
-def aln_by_dp(seq1:str, seq2:str) -> None:
+def global_aln_by_dp(seq1:str, seq2:str) -> None:
     # 来源
     UP = 1
     X = 4
     LEFT = 2
     # 罚分规则
-    gap_penalty = 0
-    mis_penalty = -4
-    match_reward = 0
+    gap_penalty = -1
+    mis_penalty = -1
+    match_reward = 2
     # 矩阵大小
     col_num = len(seq1) + 1
     row_num = len(seq2) + 1
@@ -139,7 +139,7 @@ def is_valid_aln(seq1:str, seq2:str) -> None:
     计算两序列比对结果 overlap长度和identity是否达到阈值
     '''
     print(f"{seq1}->{seq2}")
-    aln_by_dp(seq1, seq2)
+    global_aln_by_dp(seq1, seq2)
 
 
 # 序列及其反向互补两两比对 计算重叠区identity
